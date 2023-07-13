607f1157a313818789edc50589a1c7ec0483f790
Cheau <liqinb@csair.com>
2023-07-13 10:32:56 +0800

    Added card toggling feature.

--- src/components/Word/index.js
+++ src/components/Word/index.js
@@ -6,6 +6,7 @@ import { colors } from '../Highlight'
 import CompactCard from './CompactCard'
 import CompletedCard from './CompletedCard'
 import SkeletonCard from './SkeletonCard'
+import Toggle from './Toggle'
 
 function reduce(data) {
   let phonetics = {}
@@ -27,17 +28,19 @@ function reduce(data) {
 export default function Word({ children, index }) {
   const [word, partOfSpeech] = children.split('/')
   const [data, setData] = useState()
+  const [compact, setCompact] = useState(!!partOfSpeech)
   useEffect(async () => {
     const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     const array = await res.json()
     setData(reduce(array))
   }, [word])
-  const Card = data ? (partOfSpeech ? CompactCard : CompletedCard) : SkeletonCard
+  const Card = data ? (compact ? CompactCard : CompletedCard) : SkeletonCard
   const highlight = index % 2 === 0 ? colors.green : colors.yellow
   return (
       <div className={styles.card}>
         <span className={styles.title} style={{ background: highlight }}>{word}</span>
         <Card data={data}>{children}</Card>
+        {data && partOfSpeech && <Toggle compact={compact} onClick={setCompact} />}
       </div>
   )
 }
