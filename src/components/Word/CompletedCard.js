787caa0d4c51c1db870fea42ce2599ce2a55d8f9
Cheau <liqinb@csair.com>
2023-07-13 12:24:04 +0800

    Optimized look and feel.

--- src/components/Word/CompletedCard.js
+++ src/components/Word/CompletedCard.js
@@ -3,11 +3,26 @@ import React from 'react'
 import styles from './styles.module.css'
 import Phonetics from './Phonetics'
 
+function Number({ children }) {
+  return <span className={styles.number}>{children}</span>
+}
+
+function Meaning({ data, order }) {
+  const { definition, matched } = data
+  const className = matched ? styles.matched : undefined
+  const title = matched ? '当前释义' : undefined
+  return (
+      <div className={className} title={title}>
+        <Number>{order + 1}</Number>{definition}
+      </div>
+  )
+}
+
 export default function CompletedCard({ data }) {
   const groups = Object.entries(data.meanings).map(([abbr, meanings]) => (
       <div key={abbr} className={styles.meanings}>
         <div className={styles.pos}>{abbr}</div>
-        {meanings.map((m, i) => <div key={`${abbr}-${i}`}>{i + 1}. {m.definition}</div>)}
+        {meanings.map((m, i) => <Meaning key={`${abbr}-${i}`} data={m} order={i} />)}
       </div>
   ))
   return (
--- src/components/Word/index.js
+++ src/components/Word/index.js
@@ -26,13 +26,15 @@ function reduce(data) {
 }
 
 export default function Word({ children, index }) {
-  const [word, partOfSpeech] = children.split('/')
+  const [word, partOfSpeech, defIndex = 1] = children.split('/')
   const [data, setData] = useState()
   const [compact, setCompact] = useState(!!partOfSpeech)
   useEffect(async () => {
     const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     const array = await res.json()
-    setData(reduce(array))
+    const obj = reduce(array)
+    if (partOfSpeech) obj['meanings'][partOfSpeech][defIndex - 1]['matched'] = true
+    setData(obj)
   }, [word])
   const Card = data ? (compact ? CompactCard : CompletedCard) : SkeletonCard
   const highlight = index % 2 === 0 ? colors.green : colors.yellow
