f3d23526189f570ed275539567d6dbf5e5eb3d77
Cheau <liqinb@csair.com>
2023-07-13 12:23:13 +0800

    Optimized hint.

--- src/components/Word/Toggle.js
+++ src/components/Word/Toggle.js
@@ -8,7 +8,7 @@ export default function Toggle({ compact, onClick }) {
       <div
           className={styles.toggle}
           onClick={() => onClick(!compact)}
-          title={`显示${compact ? '更多' : '当前'}释义`}
+          title={`切换到${compact ? '更多' : '当前'}释义`}
       >
         {compact ? <FaChevronCircleDown /> : <FaChevronCircleUp />}
       </div>
