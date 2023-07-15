6e07d3ea0d44dd700273aa50b316e7fccabcee62
Cheau <liqinb@csair.com>
2023-07-15 11:50:49 +0800

    Fixed word not found error.

---
+++ src/components/Word/EmptyCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './styles.module.css'
+
+export default function EmptyCard() {
+  return (
+      <div className={styles.groups}>Sorry, the definition could not be found.</div>
+  )
+}
--- src/components/Word/index.js
+++ src/components/Word/index.js
@@ -5,6 +5,7 @@ import { abbreviate } from './PartOfSpeech'
 import { colors } from '../Highlight'
 import CompactCard from './CompactCard'
 import CompletedCard from './CompletedCard'
+import EmptyCard from './EmptyCard'
 import SkeletonCard from './SkeletonCard'
 import Toggle from './Toggle'
 
@@ -25,18 +26,28 @@ function reduce(data) {
   return { phonetics, meanings }
 }
 
+function getCard(data, compact) {
+  if (data === undefined) return SkeletonCard
+  if (data === null) return EmptyCard
+  return compact ? CompactCard : CompletedCard
+}
+
 export default function Word({ children, color }) {
   const [word, partOfSpeech, defIndex = 1] = children.split('/')
   const [data, setData] = useState()
   const [compact, setCompact] = useState(!!partOfSpeech)
   useEffect(async () => {
     const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    if (!res.ok) {
+      setData(null)
+      return
+    }
     const array = await res.json()
     const obj = reduce(array)
     if (partOfSpeech) obj['meanings'][partOfSpeech][defIndex - 1]['matched'] = true
     setData(obj)
   }, [word])
-  const Card = data ? (compact ? CompactCard : CompletedCard) : SkeletonCard
+  const Card = getCard(data, compact)
   return (
       <div className={styles.card}>
         <span className={styles.title} style={{ background: color ? colors[color] : 'unset' }}>{word}</span>
