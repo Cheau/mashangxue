850295d12c5b08d61d14253e922e961c6c62a0a9
cheau <cheau.ee@gmail.com>
2023-07-17 22:58:57 +0800

    Added badge feature.

--- src/components/Gallery/Placard.js
+++ src/components/Gallery/Placard.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css'
 
 export default function Placard(props) {
   const {
-     attr, channel, desc, image, link, ratio = 1, title,
+     attr, badge, channel, desc, image, link, ratio = 1, title,
   } = props
   return (
       <div className={styles.placard} style={{ paddingTop: `${ratio * 100}%` }}>
@@ -12,6 +12,7 @@ export default function Placard(props) {
           <div className={styles.container} onClick={() => window.location.href = link}>
             <img src={image} alt={title} />
             <div className={styles.channel}>{channel}</div>
+            <div className={styles.ribbon}>{badge}</div>
             <div className={styles.main}>
               <div className={styles.title}>{title}</div>
               <div className={styles.desc}>{desc}</div>
