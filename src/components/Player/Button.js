c780aa63491a1ebc343ccdf378a685e723299469
cheau <cheau.ee@gmail.com>
2023-07-20 22:29:56 +0800

    Declared to avoid error: Received `true` for a non-boolean attribute `hovering`.

--- src/components/Player/Button.js
+++ src/components/Player/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export default function Button(props) {
-  const { children, ...rest } = props
+  const { children, hovering, ...rest } = props
   return (
       <div className={styles.block} {...rest}>
         {children}
