e776db9a104568eb7fde2993643b41fc8b4ac146
Cheau <liqinb@csair.com>
2023-07-30 10:07:53 +0800

    Narrowed gaps and shortened texts.

--- src/components/HomepageFeatures/Landscape.js
+++ src/components/HomepageFeatures/Landscape.js
@@ -10,9 +10,9 @@ import styles from './landscape.module.css'
 
 const data = [
   { icon: <FcSearch />, title: '快速查单词', subtitle: '选中页面单词即可查询，用鼠标双击可快速选中' },
-  { icon: <FcClock />, title: '瞻前又顾后', subtitle: '在连续查词的过程中，会为你保留查询记录，方便来回查看' },
-  { icon: <FcSmartphoneTablet />, title: '学习尽掌握', subtitle: '页面对手机和平板电脑进行了适配，让你随时随地，想学就学' },
-  { icon: <FcCloseUpMode />, title: '技术促进步', subtitle: '作为技术人，始终相信可以用技术辅助学习，更多功能持续开发中...' },
+  { icon: <FcClock />, title: '瞻前又顾后', subtitle: '在你连续查词时，可通过查询记录来回顾' },
+  { icon: <FcSmartphoneTablet />, title: '学习尽掌握', subtitle: '页面适配手机和平板电脑，让你随时随地想学就学' },
+  { icon: <FcCloseUpMode />, title: '技术促进步', subtitle: '用技术辅助学习，更多功能持续开发中...' },
 ]
 
 export default function Landscape() {
