a63e0f40bcf68b7773f487513318c17f84ae8e81
Cheau <liqinb@csair.com>
2023-08-01 08:06:35 +0800

    Fixed not centered issue.

--- src/components/QrCode/index.js
+++ src/components/QrCode/index.js
@@ -6,9 +6,11 @@ import Alipay from './Alipay'
 export default function QrCode() {
   return (
       <div className={styles.qrCode}>
-        <Alipay horizontal={false}>
-          <div className={styles.domain}>网站访问地址<br />MaShangXue.xyz</div>
-        </Alipay>
+        <div className={styles.centered}>
+          <Alipay horizontal={false}>
+            <div className={styles.domain}>网站访问地址<br />MaShangXue.xyz</div>
+          </Alipay>
+        </div>
       </div>
   )
 }
