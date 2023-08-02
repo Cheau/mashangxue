d6102bcba7146186dee7ee4f405a378692bcdabd
cheau <cheau.ee@gmail.com>
2023-08-02 19:40:06 +0800

    Fixed build error.

--- src/components/QrCode/Url.js
+++ src/components/QrCode/Url.js
@@ -5,6 +5,7 @@ import logo from '@site/static/img/logo.png'
 import styles from './styles.module.css'
 
 export default function Url() {
+  const url = typeof window === 'undefined' ? 'https://mashangxue.xyz' : window.location.href
   return (
       <div className={styles.url}>
         <div>
@@ -12,7 +13,7 @@ export default function Url() {
               logoImage={logo}
               logoPadding={6}
               qrStyle="dots"
-              value={window.location.href}
+              value={url}
           />
           <div className={styles.slogan}>
             访问网页，扫一扫<br/>
