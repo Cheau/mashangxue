29d28f2d453fb04095352096157d1172d88f314f
Cheau <liqinb@csair.com>
2023-08-05 12:18:44 +0800

    Populated words before components rendering to avoid not triggering re-rendering.

--- src/templates/Dialog/ref.js
+++ src/templates/Dialog/ref.js
@@ -2,25 +2,28 @@ import React from 'react'
 
 import Highlight from '../../components/Highlight'
 
-const ref = (token, global) => function Ref() {
-  const { text } = token
+const ref = (token, global) => {
+  const {text} = token
   const [word, ...rest] = text.substring(1, text.length - 1).split('/')
   const [variant, origin = variant] = word.split(':')
-  const { words } = global
+  const {words} = global
   words.push({
     text: `${origin}/${rest.join('/')}`,
     word: origin,
   })
   const isOdd = words.length % 2 === 1
   words[words.length - 1].color = isOdd ? 'green' : 'yellow'
-  return (
-      <Highlight green={isOdd} yellow={!isOdd} onClick={() => {
-        const e = document.getElementById(origin || variant)
-        if (e) e.parentElement.scrollTo({ top: 0, left: e.offsetLeft - 20, behavior: 'smooth' })
-      }}>
-        {variant}
-      </Highlight>
-  )
+  return function Ref() {
+    return (
+        <Highlight green={isOdd} yellow={!isOdd} onClick={() => {
+          const e = document.getElementById(origin || variant)
+          if (e) e.parentElement.scrollTo(
+              {top: 0, left: e.offsetLeft - 20, behavior: 'smooth'})
+        }}>
+          {variant}
+        </Highlight>
+    )
+  }
 }
 
 export default ref
