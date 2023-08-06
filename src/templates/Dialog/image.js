d5df92a818a1a547a046df2fd852b7872ef6bae5
cheau <cheau.ee@gmail.com>
2023-08-06 21:26:50 +0800

    Removed invalid argument.

--- src/templates/Dialog/image.js
+++ src/templates/Dialog/image.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Image from '../../components/Image'
 
-const image = (token) => function Img({ lexeme }) {
+const image = (token) => function Img() {
   const { text } = token
   const [, alt, src] = /^!\[(.*)]\((.+)\)$/.exec(text)
   return <Image alt={alt} src={src} ratio={0.5} />
