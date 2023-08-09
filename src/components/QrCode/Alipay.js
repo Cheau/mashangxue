3630dd59aa481d45d16ab5626fab12d8422541d2
Cheau <liqinb@csair.com>
2023-08-09 12:10:50 +0800

    Updated slogan.

--- src/components/QrCode/Alipay.js
+++ src/components/QrCode/Alipay.js
@@ -7,7 +7,7 @@ import Portrait from './Portrait'
 export default function Alipay(props) {
   return (
       <div className={styles.alipay}>
-        <Portrait image={image} slogan="支付宝，扫一扫\n感恩您点滴资助" {...props} />
+        <Portrait image={image} slogan="支付宝，扫一扫\n你的一点鼓励\n是我持续创作动力" {...props} />
       </div>
   )
 }
