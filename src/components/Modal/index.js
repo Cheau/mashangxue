00b3655f8f7334e10b72c6823fe43f640dbf480d
Cheau <liqinb@csair.com>
2023-08-15 08:55:22 +0800

    Executed modal hiding only once, otherwise the scrollY will be incorrect in the second run.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -5,38 +5,42 @@ import { BiX } from 'react-icons/bi'
 import styles from './styles.module.css'
 import { useSession } from '../../common/hooks'
 
+const show = () => {
+  const { scrollY } = window
+  const { position, top, width } = document.body.style
+  document.body.style.position = 'fixed'
+  document.body.style.top = `-${scrollY}px`
+  document.body.style.width = '100%'
+  return { position, top, width }
+}
+
+const hide = (context) => {
+  const scrollY = document.body.style.top || 0
+  const { position, top, width } = context
+  document.body.style.position = position
+  document.body.style.top = top
+  document.body.style.width = width
+  window.scrollTo(0, parseInt(scrollY || '0') * -1)
+}
+
 export default function Modal({ children, onClose, open = false }) {
   const [isOpen, setIsOpen] = useState()
   const [context, setContext] = useSession('modal')
-  const close = () => {
-    setIsOpen(false)
-    const scrollY = document.body.style.top || 0
-    const { position, top, width } = context
-    document.body.style.position = position
-    document.body.style.top = top
-    document.body.style.width = width
-    window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    setContext()
-    if (typeof onClose === 'function') onClose()
-  }
   useEffect(() => {
     setIsOpen(open)
-    if (open) {
-      const { scrollY } = window
-      const { position, top, width } = document.body.style
-      setContext({ position, top, width })
-      document.body.style.position = 'fixed'
-      document.body.style.top = `-${scrollY}px`
-      document.body.style.width = '100%'
-    }
-    return close
+    if (open) setContext(show())
   }, [open])
+  useEffect(() => {
+    if (isOpen !== false) return
+    hide(context)
+    if (typeof onClose === 'function') onClose()
+  }, [isOpen])
   if (!isOpen) return null
   const modal = (
-      <div className={styles.backdrop} onClick={close}>
+      <div className={styles.backdrop} onClick={() => setIsOpen(false)}>
         <div className={styles.wrapper} onClick={(e) => e.stopPropagation()}>
           {children}
-          <span className={styles.close} onClick={close}><BiX /></span>
+          <span className={styles.close} onClick={() => setIsOpen(false)}><BiX /></span>
         </div>
       </div>
   )
