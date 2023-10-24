70ea91aaa62a11b9a94614c5a40f509bf8ee08b3
cheau <cheau.ee@gmail.com>
2023-10-24 19:19:00 +0800

    Hid features in sidebar on home page.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,11 +8,12 @@ import Features from '../HomepageFeatures/Tile'
 
 export default function Sidebar() {
   const presenting = usePresenting()
+  const { pathname } = window.location
   return (
       <div className={clsx('sidebar', styles.sidebar)}>
         <div className={clsx(styles.centered, { [styles.presenting]: presenting.get() })}>
           <QrCode />
-          <Features />
+          {pathname !== '/' && <Features />}
         </div>
       </div>
   )
