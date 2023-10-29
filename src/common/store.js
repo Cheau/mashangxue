1c1fa2ec75d51202fe9bcd34cf00fbca343d5130
cheau <cheau.ee@gmail.com>
2023-10-29 11:13:05 +0800

    Fixed localStorage not defined error.

--- src/common/store.js
+++ src/common/store.js
@@ -1,7 +1,19 @@
 import { hookstate } from '@hookstate/core'
 import { localstored } from '@hookstate/localstored'
 
-export const presenting = hookstate(false, localstored({ key: 'presenting' }))
+const localStorage = {
+  data: {},
+  getItem: (key) => this.data[key],
+  setItem: (key, value) => this.data[key] = value,
+  removeItem: (key) => delete this.data[key],
+}
+
+const local = (key) => {
+  const engine = typeof window === 'undefined' ? localStorage : window.localStorage
+  return localstored({ key, engine })
+}
+
+export const presenting = hookstate(false, local('presenting'))
 
 export default {
   presenting
